9e37d5b6cc1bc5db335e1b628b9f97c47c8ea4d9
Robert Zondervan <robert@conduction.nl>
2024-09-25 14:01:11 +0200

    Rename synchronization service, split generic code and specific defaults

---
+++ src/Service/VrijBrpService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CommonGateway\VrijBRPToZGWBundle\Service;
+
+class VrijBrpService
+{
+    public function setVrijBRPDefaults(array $configuration): array
+    {
+        if (isset($configuration['endpoint']) === false) {
+            $configuration['endpoint'] = '/v1/api/dossiers/search';
+        }
+        if (isset($configuration['method']) === false) {
+            $configuration['method'] = 'POST';
+        }
+        if (isset($configuration['types']) === false) {
+            $configuration['types'] = [
+                'intra_mun_relocation',
+                'inter_mun_relocation',
+            ];
+        }
+
+        return $configuration;
+    }
+}
