e347b941d9548852bd23f239a8f94ed48b3e500b
Robert Zondervan <robert@conduction.nl>
2024-09-26 17:22:09 +0200

    Notifications first draft
    warning: this will cause infinite loops. Working on that

---
+++ src/ActionHandler/NotificationUpdateHandler.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace CommonGateway\VrijBRPToZGWBundle\ActionHandler;
+
+use CommonGateway\VrijBRPToZGWBundle\Service\NewSynchronizationService;
+use CommonGateway\VrijBRPToZGWBundle\Service\VrijBrpService;
+use CommonGateway\CoreBundle\ActionHandler\ActionHandlerInterface;
+
+class NotificationUpdateHandler implements ActionHandlerInterface
+{
+
+    public function __construct(
+        private readonly VrijBrpService $vrijBrpService,
+    ) {
+
+    }//end __construct()
+
+
+    /**
+     *  This function returns the requered configuration as a [json-schema](https://json-schema.org/) array.
+     *
+     * @throws array a [json-schema](https://json-schema.org/) that this  action should comply to
+     */
+    public function getConfiguration(): array
+    {
+        return [
+            '$id'         => 'https://commongateway.nl/ActionHandler/SynchronizationCollectionHandler.ActionHandler.json',
+            '$schema'     => 'https://docs.commongateway.nl/schemas/ActionHandler.schema.json',
+            'title'       => 'SynchronizationCollectionHandler',
+            'description' => '',
+            'required'    => [],
+            'properties'  => [],
+        ];
+
+    }//end getConfiguration()
+
+
+    /**
+     * Run the actual business logic in the appropriate server.
+     *
+     * @param array $data          The data from the call
+     * @param array $configuration The configuration of the action
+     *
+     * @return array
+     */
+    public function run(array $data, array $configuration): array
+    {
+        return $this->vrijBrpService->createNotification($data, $configuration);
+
+    }//end run()
+
+
+}//end class
--- src/Service/VrijBrpService.php
+++ src/Service/VrijBrpService.php
@@ -2,9 +2,23 @@
 
 namespace CommonGateway\VrijBRPToZGWBundle\Service;
 
+use App\Entity\ObjectEntity;
+use CommonGateway\CoreBundle\Service\CacheService;
+use CommonGateway\CoreBundle\Service\GatewayResourceService;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+
 class VrijBrpService
 {
 
+
+    public function __construct(
+        private readonly CacheService $cacheService,
+        private readonly GatewayResourceService $resourceService,
+        private readonly EntityManagerInterface $entityManager,
+    ) {
+
+    }
 
     public function setVrijBRPDefaults(array $configuration): array
     {
@@ -28,6 +42,63 @@ class VrijBrpService
         return $configuration;
 
     }//end setVrijBRPDefaults()
+
+    function arrayRecursiveDiff($aArray1, $aArray2) {
+        $aReturn = array();
+
+        foreach ($aArray1 as $mKey => $mValue) {
+            if (array_key_exists($mKey, $aArray2)) {
+                if (is_array($mValue)) {
+                    $aRecursiveDiff = $this->arrayRecursiveDiff($mValue, $aArray2[$mKey]);
+                    if (count($aRecursiveDiff)) { $aReturn[$mKey] = $aRecursiveDiff; }
+                } else {
+                    if ($mValue != $aArray2[$mKey]) {
+                        $aReturn[$mKey] = $mValue;
+                    }
+                }
+            } else {
+                $aReturn[$mKey] = $mValue;
+            }
+        }
+        return $aReturn;
+    }
+
+    public function createNotification(array $data, array $config): array
+    {
+        var_dump('hello');
+
+
+        $object = $data['object'];
+
+        if($object instanceof ObjectEntity) {
+            $data    = $object->toArray(['embedded' => true, 'user' => $this->cacheService->getObjectUser(objectEntity: $object)]);
+            $oldData = $this->cacheService->getObject($object->getId());
+
+            $diff = $this->arrayRecursiveDiff($data, $oldData);
+
+            if(array_key_exists(key: 'status', array: $diff) === true) {
+                $now = new DateTime();
+                $message = [
+                    'kanaal'       => 'zaak.status.created',
+                    'hoofdobject'  => $data['url'],
+                    'resource'     => 'Zaak',
+                    'resourceUrl'  => $data['url'],
+                    'actie'        => 'create',
+                    'aanmaakdatum' => $now->format('c'),
+                ];
+                $schema        = $this->resourceService->getSchema(
+                    reference:'https://zgw.opencatalogi.nl/schema/nrc.message.schema.json',
+                    pluginName: 'common-gateway/vrijbrp-to-zgw-bundle'
+                );
+                $messageObject = new ObjectEntity(entity: $schema);
+                $messageObject->hydrate($message);
+                $this->entityManager->persist($messageObject);
+            }
+        }
+
+        return $data;
+
+    }
 
 
 }//end class
